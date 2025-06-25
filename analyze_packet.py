d916551361dc06b125184c18277d98dd81a26d4c
Molly Draven <drazi@duck.com>
2025-06-25 09:05:51 -0400

    fix: Remove duplicate license header in analyze_packet.py
    - Fix duplicate GPL license header lines (16-21) that were erroneously repeated
    - Addresses CodeRabbit PR review comment about license header duplication
    - Maintains proper GPL v3.0 license compliance

--- analyze_packet.py
+++ analyze_packet.py
@@ -14,11 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 NPS Packet Analyzer CLI Tool
